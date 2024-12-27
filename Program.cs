a10afaf6135f6398b40f92777050b13c8bbf8925
GFigoCh <gfigoch@outlook.com>
2024-12-27 18:15:51 +0700

    Getting Started: Creating a Window.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using (var game = new Game(800, 600, "LearnOpenGL"))
+        {
+            game.Run();
+        }
     }
 }
